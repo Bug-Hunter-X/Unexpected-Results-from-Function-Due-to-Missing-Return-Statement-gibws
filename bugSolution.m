5c8de0cd36dbb974f277ad012b359138c2ce808b
BugHunter-X <askzenai@gmail.com>
2025-01-02 13:10:04 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,16 @@
+function result = myFunction(input)
+  % Some code here...
+  if input < 0
+    result = -1; % Handle negative input
+    return; % This line was missing, causing the bug
+  end
+
+  % More code...
+  result = someCalculation(input);
+  % ...more code...
+end
+
+% Example of the function being called
+inputVal = -5;
+output = myFunction(inputVal);
+disp(output); 
